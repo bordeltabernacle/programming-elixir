5b1a45bdadab2068c71fce0f783941177b046517
Rob Phoenix <robertphoenix@gmail.com>
2016-01-20 15:26:07 +0000

    completed first 3 exercises, have to go to code club now

---
+++ chapter7.exs
@@ -0,0 +1,33 @@
+# ListsAndRecursion-0..3
+defmodule MyList do
+
+  def sum([]), do: 0
+  def sum([h|t]), do: h + sum(t)
+
+  def map([], _), do: []
+  def map([h|t], func), do: [func.(h)|map(t, func)]
+
+  def mapsum(list, func) do
+    list
+    |> map(func)
+    |> sum
+  end
+
+  def max([h|t]), do: _max(t, h)
+
+  defp _max([], acc), do: acc
+  defp _max([h|t], acc) when h > acc, do: _max(t, h)
+  defp _max([h|t], acc) when h < acc, do: _max(t, acc)
+
+  def caesar(char_list, n) do
+    char_list
+    |> map(&_caesar(&1, n))
+  end
+
+  defp _caesar(c, n) when (c + n) > 122 do
+    _caesar(96 + (c - 122), n)
+  end
+  defp _caesar(c, n), do: c + n
+end
+
+
